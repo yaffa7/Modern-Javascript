d65c16647ef44604021b24e7944953dbd49d7d5d
Brendan <brendan6z@gmail.com>
2019-06-22 12:35:34 -0700

    template literals

--- template-literals.js
+++ template-literals.js
@@ -3,3 +3,28 @@ const age = 34
 const job = 'Web developer'
 const city = 'Miami'
 
+// Without template strings (es5)
+html = '<ul><li>Name: '+ name + '</li><li>Age: '+ age +'</li></ul>'
++ '<li>job: '+ job + '</li>'
+
+
+console.log(html);
+
+// With template strings (es6)
+
+function hello() {
+    return 'hello'
+}
+
+html2 = `
+<ul>
+    <li>Name: ${name}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'bigger' : 'lower'}</li>
+</ul>
+`
+console.log(html2);
